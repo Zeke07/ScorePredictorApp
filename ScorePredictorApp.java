edf02f47ff9930e2f83a19d6d4cb9c432e8b601b
Zayn Khan <93350342+Zeke07@users.noreply.github.com>
2022-06-11 22:45:55 -0500

    Update ScorePredictorApp.java

--- ScorePredictorApp.java
+++ ScorePredictorApp.java
@@ -33,21 +33,22 @@ public class ScorePredictorApp extends Application {
     public void start(Stage stage) throws IOException {
 
         List<Score> scores = new ArrayList<Score>();
-        //scores = Parser.DataLoad("/Users/zeke/Desktop/IntelliJ_Workspace/ScorePredictor/src/GPA_BIG_final.csv");
+
+        // load data from absolute path
         scores = Parser.DataLoad("/Users/zeke/Desktop/IntelliJ_Workspace/ScorePredictorApp/src/main/java/com/example/scorepredictorapp/GPA_BIG_final.csv");
         RegressionModel model = new RegressionModel();
-        model.constructModel(scores);
+        model.constructModel(scores); // create regression line using list of scores
 
         BorderPane pane = new BorderPane();
-        Scene scene = new Scene(pane, 640, 480);
+        Scene scene = new Scene(pane, 640, 480); // program panel
         stage.setTitle("SAT Score Predictor Application");
         Button exit = new Button("Quit");
         exit.setOnAction(event -> {Platform.exit();});
         pane.setBottom(exit);
         BorderPane.setAlignment(exit, Pos.BOTTOM_RIGHT);
-        Button result = new Button("Process");
+        Button result = new Button("Process");  // the button that generates output
         result.setPrefSize( 150, 70);
-        TextField field = new TextField("Enter your Weighted GPA");
+        TextField field = new TextField("Enter your Weighted GPA"); // input field
 
         Text text = new Text();
         Pane p = new Pane(result, field, text);
@@ -61,17 +62,23 @@ public class ScorePredictorApp extends Application {
         result.setLayoutX(240);
         result.setLayoutY(320);
 
-
+        // the input field normally says "Enter here..."
+        // this just empties it upon a user click
         List<Score> finalScores = scores;
         field.setOnMouseClicked(event -> {
             field.setText("");
             text.setText("");
         });
 
+        // conversely, you can click the background to reset the input field text
         p.setOnMouseClicked(e -> {
             field.setText("Enter your Weighted GPA");
             text.setText("");
         });
+
+        // the center button will probe the text-field for valid input
+        // if it is pressed
+        // if it is found, generate the predicted SAT score using the model
         result.setOnAction(event -> {
             try {
                 Scanner input = new Scanner(field.getText());
