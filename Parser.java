2fcefbcaacd509a9aea5207a58b234834f683859
Zayn Khan <93350342+Zeke07@users.noreply.github.com>
2022-06-11 22:40:01 -0500

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -7,10 +7,16 @@ package com.example.scorepredictorapp;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * parsing dataset for regression model
+ * https://www.kaggle.com/datasets/toniabiru/gpa-dataset
+ * (renamed GPA_BIG.csv)
+ * slightly modified because I noticed the value threshold was rather low (ex: 1280 for 5.0 weighted gpa)
+ * so I skewed the set a little
  *
  * @author zeke
  */
@@ -19,7 +25,7 @@ public class Parser {
     public static List<Score> DataLoad(String filename) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File(filename), "utf-8");
         scanner.nextLine();
-        String[] temp = new String[7];
+        String[] temp = new String[7]; // seven columns in the CSV, indices 0 and 1 contain the wanted information
         List<Score> toReturn = new ArrayList<Score>();
         while (scanner.hasNext())
         {
